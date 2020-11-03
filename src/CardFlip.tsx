dff1848c128941fbcfa063992753e041e81990f8
szymonrucinski <szymonrucinski@icloud.com>
2020-11-03 15:11:50 +0100

    polisehd

--- src/CardFlip.tsx
+++ src/CardFlip.tsx
@@ -2,30 +2,27 @@ import React, { useState,useLayoutEffect,useEffect } from 'react';
 import ReactDOM, { render } from 'react-dom';
 import {Player} from './Player';
 import styledComponents from 'styled-components'
-import { createGlobalStyle } from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 import ReactCardFlip from 'react-card-flip'
 
-const CardStyle = styledComponents.img`
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  width: 30%;  
+const CardStyle = styled.img`
+  width: 600px;
+  @media(max-width: 768px)
+  {
+    width: 700px;
+  }
+
 `;
 
 export const CardFlip =(props: any) =>
 {
   
-    const [isFlipped,setIsFlipped] = useState(false);
 
-    const handleClick = () =>
-    {
-         setIsFlipped(!isFlipped);
-    };
 
     return(
-    <ReactCardFlip isFlipped={isFlipped}  flipDirection="horizontal">
-                <CardStyle src={props.cardImg} onClick={handleClick}/>
-                <CardStyle src={props.flipper} onClick={handleClick}/>
+    <ReactCardFlip isFlipped={props.isFlipped}  flipDirection="horizontal">
+                <CardStyle src={props.front}/>
+                <CardStyle src={props.back}/>
 
     </ReactCardFlip>      
     )      
--- src/pop.tsx
+++ src/pop.tsx
@@ -1,46 +1,48 @@
 import React, { useState,useLayoutEffect,useEffect } from 'react';
 import player from './Player';
 import styledComponents from 'styled-components'
-import { createGlobalStyle } from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 import {CardFlip} from './CardFlip'
 import {observer} from 'mobx-react'
-
 const BackgroundLayer = createGlobalStyle`
-@font-face {
-    font-family: CARDC___;
-    src: url(./fonts/CARDC___.ttf) format('truetype'),
-  
-}
 body{
     background: green;
-    font-family: Card Characters;
-}
+    font-family: 'Ubuntu Mono', monospace;
+    display:flex;
+    justify-content: center;
+    }
 
 `;
 
-const DisplayScore = styledComponents.h1`
+const DisplayScore = styledComponents.div`
 text-align:center;
 color: white;
+font-size: 4em;
+padding-bottom: 40px;
 `;
 
-const ButtonWrapper = styledComponents.div`
-margin-top: 50px;
-text-align: center;
-display: inline-block;
-position: absolute;
-width:100%;
+const ButtonWrapper = styled.div`
+padding-top: 40px;
+
 `;
 
-const ActionButtons = styledComponents.button`
-font-size: 20px;
+
+const ActionButtons = styled.button`
+font-size: 30px;
 text-align: center; 
+height: 60px;
 background:red;
 border-radius:15px;
 outline-color:none;
-margin-left: 15px;
-margin-right: 15px;
-font-family: Card Characters;
+font-family: 'Ubuntu Mono', monospace;
 color:white;
+cursor: pointer;
+`;
+
+const Divider = styled.div`
+  width: 10px;
+  height: auto;
+  display: inline-block;
 `;
 
 export const Pop = observer((): JSX.Element => { 
@@ -48,11 +50,33 @@ export const Pop = observer((): JSX.Element => {
     //initialSetup
     let plr = player;
     let currentCard =plr.deck[plr.deck.length-1];
-   
     function handleAction(statement:boolean)
     {
         plr.nextCardGreater(currentCard,statement);
         currentCard=plr.getCardFromDeck();
+
+    }
+    const [flipped, setFlipped] = useState(false)
+    const [front, setFront] = useState(currentCard.getImg())
+    const [back, setBack] = useState('')
+    const [actualCard, setactualCard] = useState(currentCard.getImg())
+    const [previousCard, setPreviousCard] = useState('')
+
+    const handleFlip = () => 
+    {
+        if (flipped === false)
+        {
+            currentCard=plr.getCardFromDeck();
+            setBack(currentCard.getImg())
+            setFlipped(!flipped)
+            console.log(flipped)
+        }
+        else { 
+            setBack(currentCard.getImg())
+            setFlipped(!flipped)
+                
+
+        }
 
     }
 
@@ -62,16 +86,16 @@ export const Pop = observer((): JSX.Element => {
       <span>
         <DisplayScore>{plr.score}</DisplayScore>
         </span>
-        <CardFlip cardImg={currentCard.getImg()} flipper={currentCard.getBackImg()}>
+        <CardFlip isFlipped={flipped} front={front} back={back} flipDirection="horizontal">
         </CardFlip>
         <ButtonWrapper>
         <ActionButtons onClick={(e) => {
          handleAction(true);
 
         }}>Next is greater!</ActionButtons>
-
+        <Divider/>
         <ActionButtons onClick={(e) => {
-                    handleAction(false);
+                    handleAction(false); handleFlip();
 
         }}>Next is smaller!</ActionButtons>
         </ButtonWrapper>
